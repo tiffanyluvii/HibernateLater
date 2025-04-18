5be449a5908f5afcfb41bb626127d9669bcea9be
Tiffany Lu <tiffanylu123901@gmail.com>
2025-04-18 03:01:44 -0400

    added more pngs. added messages into the strings file. worked on the bottom half of the home page.

--- app/src/main/java/com/example/hibernatelater/MainActivity.kt
+++ app/src/main/java/com/example/hibernatelater/MainActivity.kt
@@ -5,11 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        buildViewByCode()
+    }
+
+    fun animateBear(){
+
+    }
+
+    fun buildViewByCode(){
+        setContentView(R.layout.activity_main)
+
+        var timer: Timer = Timer()
+        var task: ExerciseTimerTask = ExerciseTimerTask(this)
+        timer.schedule(task, 0, 700)
+    }
+
 }
+
+
