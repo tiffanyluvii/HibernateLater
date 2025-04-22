2ce0f1a46375a991a57d061372cfb700876cee68
Tiffany Lu <tiffanylu123901@gmail.com>
2025-04-22 02:16:29 -0400

    animate the bear lifting a dumbbell! Added some functions into the HomePage.kt

--- app/src/main/java/com/example/hibernatelater/HomePage.kt
+++ app/src/main/java/com/example/hibernatelater/HomePage.kt
@@ -1,7 +1,38 @@
 package com.example.hibernatelater
 
+import android.content.Context
+
 class HomePage {
-    var exercise: Boolean = false // is the user exercising currently
+    var check_exercise: Boolean = false // is the user exercising currently
+    var check_break: Boolean = false // is the user currently on break
+
+    constructor(context:Context){
+
+    }
+
+    fun startExercise(){
+        this.check_exercise = true
+    }
+
+    fun endExercise(){
+        this.check_exercise = false
+    }
+
+    fun currentlyExercising(): Boolean{
+        return this.check_exercise
+    }
+
+    fun startBreak(){
+        this.check_break = true
+    }
+
+    fun endBreak(){
+        this.check_break = false
+    }
+
+    fun currentlyOnBreak(): Boolean{
+        return this.check_break
+    }
 
 
 }
--- app/src/main/java/com/example/hibernatelater/MainActivity.kt
+++ app/src/main/java/com/example/hibernatelater/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hibernatelater
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bear_icon: View
+    private var check_exercise: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,12 +21,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun animateBear(){
-
+        if (!check_exercise){
+            bear_icon.setBackgroundResource(R.drawable.dumbbell_bear)
+        } else {
+            bear_icon.setBackgroundResource(R.drawable.dumbbell_bear_up)
+        }
+        check_exercise = !check_exercise
     }
 
     fun buildViewByCode(){
         setContentView(R.layout.activity_main)
 
+        bear_icon = findViewById(R.id.bear)
         var timer: Timer = Timer()
         var task: ExerciseTimerTask = ExerciseTimerTask(this)
         timer.schedule(task, 0, 700)
