4e56f961ba749dc254a8553fed8e10fe71c640b0
Eli <echoi04@terpmail.umd.edu>
2025-05-12 20:15:15 -0400

    connected firebase

--- app/src/main/java/com/example/hibernatelater/Adapter.kt
+++ app/src/main/java/com/example/hibernatelater/Adapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
-class Adapter(context: Context, private val users: List<MainActivity.User>) :
+class Adapter(context: Context, private val users: MutableList<MainActivity.User>) :
     ArrayAdapter<MainActivity.User>(context, R.layout.item_leaderboard, users) {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -18,8 +18,14 @@ class Adapter(context: Context, private val users: List<MainActivity.User>) :
 
             view.findViewById<TextView>(R.id.rank).text = "${position + 1}"
             view.findViewById<TextView>(R.id.name).text = user.name
-            view.findViewById<TextView>(R.id.streak).text = "${user.streakDays} days"
+            view.findViewById<TextView>(R.id.streak).text = "${user.streak} days"
 
             return view
+        }
+
+        fun update(new : List<MainActivity.User>) {
+            users.clear()
+            users.addAll(new)
+            notifyDataSetChanged()
         }
     }
